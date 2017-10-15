afe4c5dddd24c0250c567f51b8d973eaecad37de
Narek Sedrakyan <narek.sedrakyan90@gmail.com>
2017-10-15 22:31:32 +0400

    commit

---
+++ JS/index.js
@@ -0,0 +1,102 @@
+const express   = require('express');
+const mongo     = require('mongodb');
+var util        = require('util');
+var bodyParser  = require('body-parser');
+
+var ObjectId = mongo.ObjectID;
+const mongoClient    = mongo.MongoClient;
+const app            = express();
+
+var url = "mongodb://localhost:27017/db"
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoClient.connect(url,function(err, db) {
+    if (err) throw err;
+
+    console.log("Database created!");
+    // db.createCollection("users",function(err,res) {
+    //     if (err) throw err;
+    //     console.log("collection created") 
+    // })
+    // db.close();
+
+    // db.collection('users').remove({}, function(err,result) {
+    //     if (err) throw err;
+    //     console.log("all documents are removed!") 
+    // })
+
+    db.collection('users').insertMany(allusers,function(err,result) {
+
+    });
+
+    const port = 8000;
+    const host = '127.0.0.1';
+    var userOptions = {password:false};    
+    app.listen(port,host,function() {
+        console.log('We are live on ' + port);
+    });
+    
+    app.get('/users',function(request,response) {
+        db.collection('users').find({},userOptions).toArray(function(err,result){
+            if (err) throw err;
+            response.status(200).json(result);
+        })
+    })
+
+    app.get('/users/:id',function(request,response) {
+        var query = { _id: ObjectId(request.params.id) };
+        db.collection('users').findOne(query,userOptions, function(err,result) {
+            if (err) throw err;
+            response.status(200).json(result);
+        });
+    })
+
+    app.post('/register',function(request,response) {
+        var user = request.body;
+        db.collection('users').findOne({'userName':user.userName},userOptions,function(err,result) {
+            if (result) {
+               response.status(500).json({"message": "this username is busy"}) 
+            } else {
+                db.collection('users').insertOne(user,userOptions,function(err,result) {
+                    if (err) throw err;
+                    delete user.password;
+                    response.status(200).json(user);
+                })
+            }
+        })
+    })
+
+    app.post('/login',function(request,response) {
+        var query = request.body;
+        db.collection('users').findOne(query,userOptions,function(err,result) {
+            if (err) {
+                throw err;
+            } else {
+                console.log(result);
+                if (result) {
+                    response.status(200).json(query);                   
+                } else {
+                    response.status(500).json({"message":"incorrect login or password"});                    
+                }
+            }
+        })
+    })
+
+    // questions
+
+    app.get('/questions',function(req,res){
+        res.send('all questions');
+    })
+
+    app.get('/questions/:page',function(req,res){
+        res.send(util.format('questions N%d',req.params.page));
+    })
+
+    //next page
+
+    app.post('/next',function(req,res) {
+        var user = req.body;
+    })
+})
