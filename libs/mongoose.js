91d29bdab36a12f03a3831a4d50cf68104bd154d
Narek Sedrakyan <narek.sedrakyan90@gmail.com>
2017-10-27 00:26:03 +0400

    aaa

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -2,8 +2,6 @@ var mongoose = require('mongoose');
 var config = require('./config');
  
  mongoose.Promise = global.Promise;
- mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + 'db'
-     //config.get('mongoose:uri'),config.get('mongoose:options')
-    );
+ mongoose.connect(config.get('mongoose:uri'),config.get('mongoose:options'));
 
 module.exports.mongoose;
