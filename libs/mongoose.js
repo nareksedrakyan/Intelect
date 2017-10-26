c32dc68bd941685eb401331d055e0420795ecaf1
Narek Sedrakyan <narek.sedrakyan90@gmail.com>
2017-10-26 23:48:42 +0400

    commit

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 var config = require('./config');
  
  mongoose.Promise = global.Promise;
- mongoose.connect(config.get('mongoose:uri'),config.get('mongoose:options'));
+ mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + 'db'
+     //config.get('mongoose:uri'),config.get('mongoose:options')
+    );
 
 module.exports.mongoose;
