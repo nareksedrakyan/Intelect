9922ef6bf02bc4c8fe415030bf62fded99fab7be
Narek Sedrakyan <narek.sedrakyan90@gmail.com>
2017-10-31 02:12:40 +0400

    commit

---
+++ models/schemas/locationSchema.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var LocationSchema = new Schema({
+    latitude:    { type: Number, required: true },
+    longitude:   { type: Number, required: true },
+    country:     { type: String ,default: null },
+    countryCode: { type: String ,default: null },
+    city:        { type: String ,default: null },
+    zipcode:     { type: String ,default: null }
+})
+
+module.exports = LocationSchema;
--- models/user.js
+++ models/user.js
@@ -1,29 +1,27 @@
 var mongoose = require('mongoose');
+var LocationSchema = require('./schemas/locationSchema');
 var Schema = mongoose.Schema;
-var POSITIONS = ['newbie','amateur','expert','master'];
+var POSITIONS = ['Beginner','Novice','Advanced','Expert','Master','Genius'];
 
 var userSchema = new Schema({
     userName:           { type: String, required: true, unique: true },
-    firstName:          { type: String, required: true },
-    lastName:           { type: String, required: true },
+    email:              { type: String, required: true, unique: true },
+    displayName:        { type: String,required: true },
+    lastName:           { type: String },
     password:           { type: String, required: true , select: false },
+    location:           { type: LocationSchema, required: true },
     score:              { type: Number, default: 0 },
-    position:           { type: String, enum: POSITIONS ,default: 'newbie'},
+    position:           { type: String, enum: POSITIONS ,default: 'Beginner'},
     photoUrl:           { type: String },
     questionIds:        { type: [Schema.Types.ObjectId] ,default: [] },
-    categorizedScores:  { 
+    categorizedScores:  [{ 
                           category: { type: Schema.Types.ObjectId , ref: "Category" },
                           score:    { type: Number, default: 0 }
-                        },
-    victories:          [{ 
-                            contest_id: { type: Schema.Types.ObjectId },
-                            type:       { type:String, enum:['duel','tournament','playoff'] },
-                            place:      { type:String, enum:['1-st','2-nd','3-th'] },
                         }],
     admin:              { type: Boolean, default: false },
     vip:                { type: Boolean, default: false },
-    online:             { type:Boolean, default: false },
-    location:           { type: String, required: true },
+    online:             { type: Boolean, default: false },
+    location:           { type: String },
     createdAt:          { type: String },
     updatedAt:          { type: String },
     accessToken:        { type: String, default: null }
